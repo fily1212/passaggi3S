8b6c21e50320bfae7a9c051da381d38154aa236c
Filippo <10487430+fily1212@users.noreply.github.com>
2025-01-10 13:15:39 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let URL = "https://gpa.madbob.org/query.php?stop=1695";
+let URL = "https://gpa.madbob.org/query.php?stop=";
 
 function aggiungiPassaggio(linea, orario){
     let div = document.createElement("div");
@@ -12,12 +12,13 @@ function aggiungiPassaggio(linea, orario){
     document.getElementById("lista").appendChild(div);    
 }
 
-
 function mostra(lista){
     aggiungiPassaggio(lista[0].line, lista[0].hour);
     aggiungiPassaggio(lista[1].line, lista[1].hour);
 }
 
-fetch(URL)
+function cercafermata(){
+fetch(URL + document.getElementById("num").value)
 .then(x => x.json())
 .then(y => mostra(y));
+}
